97d02ffa6b514381d11a31a2bef6e277e4369331
sad19016 <saarth.desai@uconn.edu>
2026-01-30 18:22:26 -0500

    Updated Regressor.py with Scaler and MLPRegrssor code
    Added the Scaler and ran MLPRegressor model!

--- src/Regressor.py
+++ src/Regressor.py
@@ -26,3 +26,29 @@ X_train, X_test, y_train, y_test = train_test_split(
 # Quick sanity check
 print(X_train.shape, X_test.shape)
 print(y_train.shape, y_test.shape)
+
+
+# Saarth here! Now, let's take the split data and run it in MLPRegressor!
+
+# First, let's add the scaler:
+from sklearn.preprocessing import StandardScaler
+
+scaler = StandardScaler()
+
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# Now, let's run the model:
+from sklearn.neural_network import MLPRegressor
+
+mlp = MLPRegressor(
+    hidden_layer_sizes=(64, 32),
+    alpha=0.0005,
+    learning_rate_init=0.001,
+    early_stopping=True,
+    validation_fraction=0.1,
+    max_iter=1000,
+    random_state=42
+)
+
+mlp.fit(X_train_scaled, y_train)
